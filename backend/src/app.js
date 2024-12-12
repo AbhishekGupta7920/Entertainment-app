cd8de1222a87eb2b06baa2f33ff64be62894da81
Abhishek Gupta <abhigupta12558@gmail.com>
2024-12-12 14:21:36 +0530

    cors added

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,12 +10,15 @@ const { userRouter } = require('./routes/user.routes.js')
 
 // app instances 
 const app = express();
+app.use(cors());
 
 
 // Middleware 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+
 app.use(cors({
     origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
